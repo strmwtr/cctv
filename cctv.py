dd841bb96a85c08575fcd93ae6c43ecb4d943448
Bob Brown <rb3@tutanota.com>
2017-11-09 10:36:23 -0500

    Added csv reader function pipe_list

--- cctv.py
+++ cctv.py
@@ -1,12 +1,26 @@
 import os
 import shutil
 import arcpy
+import csv
 
 fldr = r'C:\Users\brownr\Desktop\Aug'
 pipes = (r'Database Connections\Connection to GISPRDDB direct connect.sde'  
   r'\cvgis.CITY.Utilities_Storm\cvgis.CITY.storm_pipe_line')
 arcpy.env.overwriteOutput = True
+data = r'C:\Users\brownr\Desktop\pipes\pipes.csv'
 
+def pipe_list(csv_file):
+  csv_list = []
+  with open(csv_file, 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+      csv_list.append(row)
+
+  pipes = []
+  for line in csv_list:
+    pipes.append(line[8])
+
+  return pipes
 
 def rename_dirs():
   for dir in os.listdir(fldr):
@@ -54,10 +68,6 @@ def sql_query():
   pipe_query = str(os.listdir(fldr))
   pipe_query = 'PIPEID in ({0})'.format(pipe_query[1:-1])
 
-  #f = open(fldr + '\\sql_query.txt', 'w')
-  #f.write(pipe_query)
-  #f.close()
-  
   arcpy.Select_analysis(pipes, fldr + r'\pipes.shp', pipe_query)
   
   updated_pipes_1 = []
@@ -73,7 +83,6 @@ def sql_query():
     rev_list.append(rev_pipe) 
   
   pipe_query_2 = 'PIPEID in ({0})'.format(str(rev_list)[1:-1])
-  #print '{0}{1}'.format(pipe_query[:-1], pipe_query_2[11:])
 
   arcpy.Select_analysis(pipes, fldr + r'\pipes_2.shp', pipe_query_2)
 
@@ -90,35 +99,5 @@ def sql_query():
   total_updates = updated_pipes_1 + rev_list
   edge_case = [pipe for pipe in all_pipes if pipe not in total_updates]
   print edge_case
-  '''
-  need_update = all_pipes
-  for pipe in all_pipes:
-    if pipe in updated_pipes:
-      need_update.remove(pipe)
-  print 'need_update r1'
-  print need_update
-  print len(need_update)
-  
-  rev_list = []
-  for pipe in need_update:
-    rev_pipe = '{0}-{1}'.format(pipe.split('-')[1], pipe.split('-')[0]) 
-    rev_list.append(rev_pipe)
 
-  pipe_query_2 = str(rev_list)
-  pipe_query_2 = 'PIPEID in ({0})'.format(pipe_query_2[1:-1])
-  arcpy.Select_analysis(pipes, fldr + r'\pipes_2.shp', pipe_query_2)
-
-  updated_pipes_2 = []
-  with arcpy.da.SearchCursor(fldr + r'\pipes_2.shp', ['PIPEID']) as cursor:
-    for row in cursor:
-      updated_pipes_2.append(str(row[0]))
-      
-  for pipe in updated_pipes_2:
-    need_update.remove('{0}-{1}'.format(pipe.split('-')[1], pipe.split('-')[0]))
-  print 'need_update r2'
-  print need_update
-  print len(need_update)
-  '''
-
-
-sql_query()
+print pipe_list(data)
